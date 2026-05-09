7d13984179a9aabdd7bd6011d16d071faa59e827
Arto Puro-Aho <130635076+artopuro-aho@users.noreply.github.com>
2026-05-09 22:26:18 +0300

    korostus.lst changed to korostus1.lst
    The PÄIVÄ parameter only determines which numbered file is opened (korostus1.lst, korostus2.lst, etc.). If you don't set PÄIVÄ, it defaults to k_pv=0 → korostus1.lst

--- TPsource/V52/Hk/HkInit.cpp
+++ TPsource/V52/Hk/HkInit.cpp
@@ -213,7 +213,8 @@ void merkisto(wchar_t merkit)
 
 void lueKorRajat(void)
    {
-   wchar_t rajaflnm[] = L"korostus.lst", buf[200], srjnm[200];
+   wchar_t rajaflnm[20], buf[200], srjnm[200];
+   swprintf(rajaflnm, L"korostus%d.lst", k_pv + 1);
    int srj, rj1;
    unsigned int rj2;
    TextFl *rajafl;
